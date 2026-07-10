ff393bc323644f38bf8c5e394e3e7e18346f50b6
Prerit Gupta <155992865+TryToPlay@users.noreply.github.com>
2026-07-10 20:42:15 +0530

    Add files via upload

---
+++ backend/src/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { setServers } from "dns/promises";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/ratelimiter.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+const __dirname = path.resolve();
+
+setServers(["0.0.0.0", "1.1.1.1", "8.8.8.8"]);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }));
+}
+app.use(express.json());
+
+app.use(rateLimiter);
+
+app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on port:", PORT);
+    });
+});
